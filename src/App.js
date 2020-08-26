3b85ae893d3b9870b3daf681a30bce993f6443ae
Дмитрий <tomountain@mail.ru>
2020-08-27 00:03:03 +0400

    refactor: move code to own alone component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import EntryField from './Components/EntryField';
 import ChooseMaterial from './Components/ChooseMaterial';
-import Checkbox from './Components/Checkbox';
-import Score from './Components/Score';
+import showCheckMar from './Helper/Checkbox';
 import './App.css';
 
+const declination = {
+  0: 'метр',
+  1: 'метра',
+  2: 'метров',
+}
+
 function App() {
-  const [height, setHeight] = useState(0);
-  const [length, setLength] = useState(0);
+  const [height, setHeight] = useState('');
+  const [length, setLength] = useState('');
   const [price, setPrice] = useState(400);
   const [result, setResult] = useState(0);
   const [mounting, setMounting] = useState(false);
@@ -15,6 +19,10 @@ function App() {
   const [farther, setFarther] = useState(false);
   const [switchPage, setSwitchPage] = useState('start');
   const [resultMaterial, setResultMaterial] = useState('');
+  const [name, setName] = useState('');
+  const [mail, setMail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const material = height * length * price;
@@ -26,7 +34,80 @@ function App() {
       setFarther(false)
     }
     setResult(sum);
-  }, [height, length, price, mounting, check])
+  }, [height, length, price, mounting, check]);
+
+  const handleUserCheck = (event) => {
+    setMounting(event.target.checked);
+  }
+
+  const handlerUserLenght = (event) => {
+    const value = event.target.value;
+    const number = value.replace(/\D/g, '');
+    setLength(number);
+  }
+
+  const handlerUserHeight = (event) => {
+    const value = event.target.value;
+    const number = value.replace(/\D/g, '');
+    setHeight(number);
+  }
+
+  const handlerUserName = (event) => {
+    const value = event.target.value;
+    const word = value.replace(/\d/g, '');
+    setName(word);
+  }
+
+  const handlerUserMail = (event) => {
+    const value = event.target.value;
+    setMail(value);
+  }
+
+  const handlerUserPhone = (event) => {
+    const value = event.target.value;
+    const number = value.replace(/\D/g, '');
+    setPhone(number);
+  }
+
+  //helper
+
+  const manyMoney = () => {
+    const str = result.toString();
+    const many = str.length;
+    switch(many) {
+      case 1:
+        return `${result} ₽`;
+        break;
+      case 2:
+        return `${result} ₽`;
+        break;
+      case 3:
+        return `${result} ₽`;
+        break;
+      case 4: 
+        return `${str[0]} ${str.slice(1, 4)} ₽`;
+        break;
+      case 5:
+        return `${str.slice(0, 2)} ${str.slice(2, 5)} ₽`;
+        break;
+      case 6:
+        return `${str.slice(0, 3)} ${str.slice(3, 6)} ₽`;
+        break;
+      case 7:
+        return `${str[0]} ${str.slice(1, 4)} ${str.slice(4, 7)} ₽`;
+        break;
+      case 8:
+        return `${str.slice(0, 2)} ${str.slice(2, 5)} ${str.slice(5, 8)} ₽`;
+        break;
+      case 9:
+        return `${str.slice(0, 3)} ${str.slice(3, 6)} ${str.slice(6, 9)} ₽`;
+        break;
+      case 10:
+        return `${str[0]} ${str.slice(1, 4)} ${str.slice(4, 7)} ${str.slice(7, 10)} ₽`;
+      default:
+        return 'Уточнить детали';
+    }
+  }
 
   const showCurrentPage = () => {
     switch(switchPage) {
@@ -35,11 +116,47 @@ function App() {
           <div className="form">
             <h1 className="form__title">Заказать забор</h1>
             <form autoComplete="off" name="fence">
-              <EntryField name="Длина забора" title="Длина забора указывается в метрах" typeField="text" classOption="fence-size__number" placeholderValue="0" sign={true} setLength={setLength}/>
-              <EntryField name="Высота забора" title="Высота забора указывается в метрах" typeField="text" classOption="fence-size__number" placeholderValue="0" sign={true} setHeight={setHeight}/>
+              <div className="form__inner">
+                <label>
+                  <h4 className="input__title">Длина забора</h4>
+                  <input
+                    type="text"
+                    className="input-wrapper"
+                    placeholder="0"
+                    value={length}
+                    onChange={handlerUserLenght}
+                    required
+                  /> 
+                </label>
+                <span className="fence-size__dimension">метров</span>
+                {showCheckMar(length.length, 0)}
+              </div>
+              <div className="form__inner">
+                <label>
+                  <h4 className="input__title">Высота забора</h4>
+                  <input
+                    type="text"
+                    className="input-wrapper"
+                    placeholder="0"
+                    value={height}
+                    onChange={handlerUserHeight}
+                    required
+                  /> 
+                </label>
+                <span className="fence-size__dimension">метров</span>
+                {showCheckMar(height.length, 0)}
+              </div>
               <ChooseMaterial setPrice={setPrice} check={check} setCheck={setCheck} setResultMaterial={setResultMaterial}/>
-              <Checkbox setMounting={setMounting}/>
-              <Score result={result}/>
+              <div className="checkbox-wrapper">
+                <label>
+                  <input type="checkbox" onChange={handleUserCheck}/>
+                    Нужен монтаж
+                </label>
+              </div>
+              <div className="price">
+                <span>Cумма заказа: </span>
+                <span className="price__number">{`${manyMoney()}`}</span>
+              </div>
               <input 
                 type="submit"
                 className={`${farther ? 'continue__on continue' : 'continue__off continue'}`}
@@ -52,7 +169,7 @@ function App() {
                     setFarther(false);
                   }
                 }
-              }/>
+              }/>             
             </form>
           </div>
         );
@@ -61,56 +178,80 @@ function App() {
         return (
           <div className="form">
             <h1 className="form__title">Пожалуйста, представьтесь</h1>
-            <EntryField 
-              name="Ваше имя"
-              classOption="contact__number"
-              placeholderValue=""
-              sign={false}
-              typeField="text"
-            />
-            <EntryField
-              name="Электронная почта"
-              classOption="contact__number"
-              placeholderValue=""
-              sign={false}
-              typeField="text"
-            />
-            <EntryField 
-              name="Телефон"
-              classOption="contact__number"
-              placeholderValue="Телефон +7 (___) ___-__-__"
-              title="Номер должен содержать 11 цифр и начинаться с 8"
-              sign={false}
-              typeField="text"
-            />
-            <div className="result">
-              Вы укомплектовали забор
-              <span className="result-sentence">{` длинной ${length} метров `}</span> и
-              <span className="result-sentence">{` высотой ${height} метров `}</span> из материала
-              <span className="result-sentence">{` ${resultMaterial.toLowerCase()} `}</span> на сумму
-              <span className="result-price">{` ${result} Р`}</span>
-            </div>
-            <div className="result-btn__wrapper">
-            <input 
-                type="submit"
-                className={`${farther ? 'continue__on data' : 'continue__off data'}`}
-                value="Далее"
-                onClick={(event) => {
-                  event.preventDefault();
-                  if (farther) {
-                    setSwitchPage('contact');
-                    setFarther(false);
+            <form autoComplete="off" name="fence">
+            <div className="form__inner input__contact">
+                <label>
+                  <h4 className="input__title">Ваше имя</h4>
+                  <input
+                    type="text"
+                    className="input-wrapper input-contact"
+                    value={name}
+                    onChange={handlerUserName}
+                    required
+                  />
+                  {showCheckMar(name.length, 1)}
+                </label>
+              </div>
+              <div className="form__inner input__contact">
+                <label>
+                  <h4 className="input__title">Электронная почта</h4>
+                  <input
+                    type="email"
+                    className="input-wrapper input-contact"
+                    value={mail}
+                    onChange={handlerUserMail}
+                    required
+                  />
+                  {showCheckMar(mail.length, 5)}
+                </label>
+              </div>
+              <div className="form__inner input__contact">
+                <label>
+                  <h4 className="input__title">Телефон</h4>
+                  <input
+                    type="tel"
+                    className="input-wrapper input-contact"
+                    value={phone}
+                    onChange={handlerUserPhone}
+                    required
+                  />
+                  {showCheckMar(phone.length, 6)}
+                </label>
+              </div>
+              <div className="result">
+                Вы укомплектовали забор
+                <span className="result-sentence">{` длинной ${length} метров `}</span> и
+                <span className="result-sentence">{` высотой ${height} метров `}</span> из материала
+                <span className="result-sentence">{` ${resultMaterial.toLowerCase()} `}</span> на сумму
+                <span className="result-price">{` ${result} Р`}</span>
+              </div>
+              <div className="input__contact">
+                <input 
+                  type="submit"
+                  className={`${farther ? 'continue__on data' : 'continue__off data'}`}
+                  value="Отправить"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    if (name.length > 1 && mail.length > 5 && phone.length > 6) {
+                      setSwitchPage('finish');
+                      setFarther(false);
+                      const num = count + 1;
+                      setCount(num)
+                    }
                   }
-                  console.log(farther)
-                }
-              }/>
-            </div>
+                }/>
+              </div>
+            </form>
           </div>
         )
       break;
       default:
         return (
-          <div className="finish"></div>
+          <div className="finish">
+            <h2 className="finish__title">{`${name}, заказ №${count} сформирован`}</h2>
+            <p className="finish__mail">{`Мы повторили его комплектацию на почту ${mail}`}</p>
+            <p className="finish__phone">{`В ближайшее время наш специалист свяжется с вами по телефону ${phone}`}</p>
+          </div>
         );
     }
   }
--- src/Helper/Checkbox/index.js
+++ src/Helper/Checkbox/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.css';
 
-const showCheckMark = (length) => (
-  <span className="checkmark">{length > 0 ? '✓' : ''}</span>
+const showCheckMark = (length, number) => (
+  <span className="checkmark">{length > number ? '✓' : ''}</span>
 );
 
 export default showCheckMark;
